cc61024bf148eff23c97db762e2b91709a4a8393
Xia Peng <131946882+KasterBodge@users.noreply.github.com>
2023-07-04 09:21:01 +0800

    Update Npc_multiple.lua

--- Npc_multiple.lua
+++ Npc_multiple.lua
@@ -1,1 +1,3 @@
-
+        Integer userId = (Integer) auth.getPrincipal();
+        this.userMedalService.updateProgress(userId, Medal.SYRY, 1, 2);
+        return ApiResponse.defaultSuccessResult("上报成功", null);
